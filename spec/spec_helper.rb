f1c6a261f42c632e2b4d2c5db802a1ff88e4fdca
Adam Mueller <adam@freerunningtech.com>
2015-05-26 13:11:59 -0700

    Remove unused factories file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,9 +29,6 @@ require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
 
-# Requires factories defined in lib/spree_signifyd/factories.rb
-require 'spree_signifyd/factories'
-
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
