9811e1fa93013ec57f16e2e7e22117f55f1fcfb8
Adam Mueller <adam@freerunningtech.com>
2015-06-10 09:53:16 -0700

    Add `null: false` to migrations.
    Will be the default in Rails 5 and throws a deprecation warning now
    otherwise.

--- db/migrate/20150206151312_create_spree_signifyd_order_scores.rb
+++ db/migrate/20150206151312_create_spree_signifyd_order_scores.rb
@@ -3,7 +3,7 @@ class CreateSpreeSignifydOrderScores < ActiveRecord::Migration
     create_table :spree_signifyd_order_scores do |t|
       t.integer :order_id
       t.integer :score
-      t.timestamps
+      t.timestamps null: true
     end
 
     add_index :spree_signifyd_order_scores, :order_id, unique: true
