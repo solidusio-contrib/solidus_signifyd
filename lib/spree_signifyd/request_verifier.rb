55fa9cde78a3f24e19f64057a71edf0cfcf4362c
Richard Nuno <me@richardnuno.com>
2015-07-31 14:18:55 -0400

    Fixes issues with requests that contain special characters.
    Change forces a UTF-8 encoding on the request data to ensure the SHA we generate is the same as the SHA sent by Signifyd.

--- lib/spree_signifyd/request_verifier.rb
+++ lib/spree_signifyd/request_verifier.rb
@@ -1,6 +1,10 @@
 module SpreeSignifyd
   module RequestVerifier
 
+    def encode_request(request_body)
+      request_body.force_encoding('ISO-8859-1').encode('UTF-8')
+    end
+
     def build_sha(key, message)
       sha256 = OpenSSL::Digest::SHA256.new
       digest = OpenSSL::HMAC.digest(sha256, key, message)
