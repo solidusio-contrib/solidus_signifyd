940d19eb9a0ffcce6105079d7954fae3fd2e4cb9
Richard Nuno <me@richardnuno.com>
2016-02-01 11:27:00 -0500

    Log order information sent to Signifyd
    There were reports of some of the information for certain orders not being sent to Signifyd.
    
    This extra logging will allow us to compare the information we’re sending against the information received by Signifyd.

--- lib/spree_signifyd/create_signifyd_case.rb
+++ lib/spree_signifyd/create_signifyd_case.rb
@@ -6,6 +6,7 @@ module SpreeSignifyd
       Rails.logger.info "Processing Signifyd case creation event: #{order_number_or_id}"
       order = Spree::Order.find_by(number: order_number_or_id) || Spree::Order.find(order_number_or_id)
       order_data = JSON.parse(OrderSerializer.new(order).to_json)
+      Rails.logger.info "Order data sent to Signifyd for order #{order.number}: #{order_data}"
       Signifyd::Case.create(order_data, SpreeSignifyd::Config[:api_key])
     end
 
