ca59ea32e3f4d3e19862cff25dd8455046ca9286
Richard Wilson <richard@freerunningtech.com>
2016-05-31 13:28:06 -0700

    Have CreateSignifydCase inherit from Active Job.

--- lib/spree_signifyd/create_signifyd_case.rb
+++ lib/spree_signifyd/create_signifyd_case.rb
@@ -1,13 +1,12 @@
 module SpreeSignifyd
-  class CreateSignifydCase
-    @queue = :spree_backend_high
+  class CreateSignifydCase < ActiveJob::Base
+    queue_as :default
 
-    def self.perform(order_number_or_id)
+    def perform(order_number_or_id)
       Rails.logger.info "Processing Signifyd case creation event: #{order_number_or_id}"
       order = Spree::Order.find_by(number: order_number_or_id) || Spree::Order.find(order_number_or_id)
       order_data = JSON.parse(OrderSerializer.new(order).to_json)
       Signifyd::Case.create(order_data, SpreeSignifyd::Config[:api_key])
     end
-
   end
 end
