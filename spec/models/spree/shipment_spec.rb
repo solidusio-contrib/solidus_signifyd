c85cb8017f61d8b0407709e3d9aee7a7b104e832
Kevin Attfield <kevin@stembolt.com>
2016-07-20 14:33:07 -0700

    Fix tautological specs
    I think these specs were intended to test "unless our override logic is
    satisfired we'd like the default Solidus behaviour" but stubbing `super`
    is hard to do, and what was here
    
    subject { shipment.determine_state(shipment.order) }
    ...
    shipment.should_receive(:determine_state).with(shipment.order)
    
    just had no way to fail. I have copied in the solidus specs for
    determine_state (with minor changes) so at least we're testing something
    here now.

--- spec/models/spree/shipment_spec.rb
+++ spec/models/spree/shipment_spec.rb
@@ -5,7 +5,7 @@ describe Spree::Shipment, :type => :model do
   let(:shipment) { create(:shipment) }
   subject { shipment.determine_state(shipment.order) }
 
-  describe "#determine_state_with_signifyd" do
+  describe "#determine_state" do
     context "with a canceled order" do
       before do
         shipment.order.update(state: 'canceled')
@@ -23,14 +23,33 @@ describe Spree::Shipment, :type => :model do
       it "pending shipments remain pending" do
         expect(subject).to eq "pending"
       end
-    end
 
-    [:shipped, :ready].each do |state|
-      context "the shipment is #{state}" do
-        before { shipment.update(state: state) }
-        it "defaults to existing behavior" do
-          expect(shipment).to receive(:determine_state).with(shipment.order)
-          subject
+      describe "regular Solidus behaviour" do
+        context "order cannot ship" do
+          before { allow(shipment.order).to receive_messages can_ship?: false }
+
+          it 'returns pending' do
+            expect(subject).to eq 'pending'
+          end
+        end
+
+        context "order can ship" do
+          before { allow(shipment.order).to receive_messages can_ship?: true }
+
+          it 'returns shipped when already shipped' do
+            allow(shipment).to receive_messages state: 'shipped'
+            expect(subject).to eq 'shipped'
+          end
+
+          it 'returns pending when unpaid' do
+            allow(shipment.order).to receive_messages paid?: false
+            expect(subject).to eq 'pending'
+          end
+
+          it 'returns ready when paid' do
+            allow(shipment.order).to receive_messages paid?: true
+            expect(subject).to eq 'ready'
+          end
         end
       end
     end
