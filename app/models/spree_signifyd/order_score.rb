b4fa2f86548abeb83f9e5f022125b3db8afb4d5d
Frederic Boivin <frederic@stembolt.com>
2016-09-28 11:48:56 -0700

    Add link to Signifyd dashboard from backend.
    -- Add preference to store the Signifyd url
    -- Add method to safely build the link from the caseId
    -- Add a partial to display the link
    -- Add test coverage to the OrderScore class

--- app/models/spree_signifyd/order_score.rb
+++ app/models/spree_signifyd/order_score.rb
@@ -1,9 +1,18 @@
 module SpreeSignifyd
   class OrderScore < ActiveRecord::Base
+    class MissingCaseId < StandardError; end
     self.table_name = :spree_signifyd_order_scores
 
     belongs_to :order, class_name: "Spree::Order"
     validates :score, numericality: true
     validates :order, presence: true
+
+    def build_case_url
+      signifyd_url = SpreeSignifyd::Config[:signifyd_dashboard_case_url]
+
+      raise MissingCaseId if case_id.blank?
+
+      signifyd_url.gsub(/:case_id/, ERB::Util.url_encode(case_id))
+    end
   end
 end
---
+++ spec/models/spree_signifyd/order_score_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe SpreeSignifyd::OrderScore, type: :model do
+  let!(:order) { create(:order_ready_to_ship, line_items_count: 1) }
+  let!(:order_score) { described_class.create!(order: order, score: 100, case_id: 1) }
+
+  describe '#build_case_url' do
+    subject { order_score.build_case_url }
+
+    context 'no case_id' do
+      let!(:order_score) { described_class.create!(order: order, score: 100, case_id: nil) }
+
+      it 'will raise a MissingCaseId error' do
+        expect{ subject }.to raise_error(SpreeSignifyd::OrderScore::MissingCaseId) 
+      end
+    end
+
+    context 'case_id and signifyd_dashboard_case_url' do
+      it 'uses the url from the config to determine url' do
+        expect(subject).to match('https://app.signifyd.com/cases/1')
+      end
+    end
+  end
+end
