f3a141496396a300c3f35af1a320beecb90140d7
Kevin Attfield <k.attfield@gmail.com>
2018-12-18 20:03:23 -0800

    Always expect args when defining attributes
    I guess this changed.

--- app/serializers/spree_signifyd/billing_address_serializer.rb
+++ app/serializers/spree_signifyd/billing_address_serializer.rb
@@ -2,7 +2,7 @@ require 'active_model_serializers'
 
 module SpreeSignifyd
   class BillingAddressSerializer < AddressSerializer
-    def attributes
+    def attributes(*args)
       hash = {}
       hash['billingAddress'] = address
       hash
--- app/serializers/spree_signifyd/delivery_address_serializer.rb
+++ app/serializers/spree_signifyd/delivery_address_serializer.rb
@@ -2,7 +2,7 @@ require 'active_model_serializers'
 
 module SpreeSignifyd
   class DeliveryAddressSerializer < AddressSerializer
-    def attributes
+    def attributes(*args)
       hash = {}
       hash['deliveryAddress'] = address
       hash['fullName'] = object.full_name
